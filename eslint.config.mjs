e6263d912c93fd8e57f92753644ddec189fcc24c
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-05-08 01:22:34 +0900

    ESLitnt 更新

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,34 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import pluginTs from "@typescript-eslint/eslint-plugin"; // TypeScriptプラグインのインポート
+import tsParser from "@typescript-eslint/parser"; // TypeScriptパーサーのインポート
 
 export default [
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
+  // TypeScriptファイル用の設定
+  {
+    files: ["*.ts", "*.tsx"], // TypeScriptファイルを指定
+    languageOptions: {
+      parser: tsParser, // TypeScript用のパーサーを指定
+      ecmaVersion: 2020, // ECMAScriptのバージョンを指定
+      sourceType: "module", // モジュールタイプを指定
+      ecmaFeatures: {
+        jsx: true, // JSXのサポートを有効にする
+      },
+    },
+    plugins: [pluginTs], // TypeScriptプラグインを使用
+    extends: [
+      pluginTs.configs.recommended, // TypeScript推奨設定を適用
+    ],
+    rules: {
+      // TypeScript固有のルール設定
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
 ];
