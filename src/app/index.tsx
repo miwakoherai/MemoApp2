4bbdc5f2fedbc02f01e24fea7adeea2a0c8f68e9
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-05-07 15:29:09 +0900

    Complete memoListItem styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,37 +10,43 @@ const Index = (): JSX.Element => {
           <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
-     <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2023年5月7日 12:00</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2023年5月7日 11:00</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
-      <View>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2023年5月7日 13:00</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
 
+     <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年5月7日 12:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>        
+        </View>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年5月7日 12:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>        
+        </View> 
+        
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年5月7日 12:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>        
+        </View>       
+      </View>
+      
       <View>
         <Text>+</Text>
       </View>
+
     </View>
   )
 }
@@ -70,9 +76,26 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     fontWeight: 'bold',
     color: '#ffffff'
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)' 
+  },
+  memoListItemTitle:{
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate:{
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
   }
-}
-
-)
+})
 export default Index
 
