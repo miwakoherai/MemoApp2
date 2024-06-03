7fc0bb8b24c3cf81176af5fab2e035508e1b83ed
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-06-03 17:11:00 +0900

    Copy code Implement functionality to register memos and dates per user

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -5,15 +5,21 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 import { router } from "expo-router";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
 
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
-import { db } from "../../config";
+import { db, auth } from "../../config";
+import { useState } from "react";
 
-const handlePress = (): void => {
-  addDoc(collection(db, "memos"), {
-    bodyText: "test",
+const handlePress = (bodyText: string): void => {
+  if (auth.currentUser === null) {
+    return;
+  }
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+  addDoc(ref, {
+    bodyText,
+    updateAt: Timestamp.fromDate(new Date()),
   })
     .then((docRef) => {
       console.log("success", docRef.id);
@@ -25,12 +31,24 @@ const handlePress = (): void => {
 };
 
 const Create = (): JSX.Element => {
+  const [bodyText, setBodyText] = useState("");
   return (
     <KeyboardAvoidingView behavior="height" style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput multiline style={styles.input} value="" />
+        <TextInput
+          multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(text) => {
+            setBodyText(text);
+          }}
+        />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton
+        onPress={() => {
+          handlePress(bodyText);
+        }}
+      >
         <Icon name="check" size={40} color="#ffffff" />
       </CircleButton>
     </KeyboardAvoidingView>
