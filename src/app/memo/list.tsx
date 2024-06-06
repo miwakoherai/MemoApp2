5fdef80f11a9c7e320e072448c87bd25813e9888
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-06-06 17:42:28 +0900

    取得したデータを表示できるように実装

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from "react-native";
 import { router, useNavigation } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 
 import MemoListItem from "../../components/MemoListItem";
@@ -8,11 +8,13 @@ import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
 import LogOutButton from "../../components/LogOutButton";
 import { db, auth } from "../../config";
+import { type Memo } from "../../../types/memo";
 
 const handlePress = (): void => {
   router.push("/memo/create");
 };
 const List = (): JSX.Element => {
+  const [memos, setMemos] = useState<Memo[]>([]);
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
@@ -27,19 +29,36 @@ const List = (): JSX.Element => {
     }
     const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
     const q = query(ref, orderBy("updatedAt", "desc"));
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      snapshot.forEach((doc) => {
-        console.log("memo", doc.data());
-      });
-    });
+    console.log("before onSnapshot");
+    const unsubscribe = onSnapshot(
+      q,
+      (snapshot) => {
+        const remoteMemos: Memo[] = [];
+        console.log(JSON.stringify(snapshot, null, 2));
+        snapshot.forEach((doc) => {
+          console.log("memo", doc.data());
+          const { bodyText, updatedAt } = doc.data();
+          remoteMemos.push({
+            id: doc.id,
+            bodyText,
+            updatedAt,
+          });
+        });
+        setMemos(remoteMemos);
+      },
+      (error) => {
+        console.error("Firestore error: ", error);
+      }
+    );
+
     return unsubscribe;
   }, []);
   return (
     <View style={styles.container}>
       <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
+        {memos.map((memo) => (
+          <MemoListItem key={memo.id} memo={memo} />
+        ))}
       </View>
       <CircleButton onPress={handlePress}>
         <Icon name="plus" size={40} color="#ffffff" />
